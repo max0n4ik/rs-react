278ff0061d4cefdaba2f472d1eaac958f772f2a4
max0n4ik <[max2004.gorohov@yandex.ru]>
2025-07-09 17:49:59 +0300

    build: add basic config files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
+const rootElement =
+  document.querySelector('#root') ??
+  ((): HTMLDivElement => {
+    const element = document.createElement('div');
+    element.id = 'root';
+    document.body.append(element);
+    return element;
+  })();
+
+createRoot(rootElement).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <h1>Hello World!!</h1>
+  </StrictMode>
+);
